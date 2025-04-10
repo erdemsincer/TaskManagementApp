2258f54f9852d78df2b4aa33b62946671f76e696
Erdem <erdemsincer123@icloud.com>
2025-04-11 02:23:02 +0300

    Task ıtem controller

---
+++ TaskManagementApp.WebAPI/Controllers/TaskItemController.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementApp.BusinessLayer.Features.Mediator.Commands.TaskItemCommands;
+using TaskManagementApp.BusinessLayer.Features.Mediator.Queries.TaskltemQueries;
+
+namespace TaskManagementApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskItemController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TaskItemController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TaskItemList()
+        {
+            var values = await _mediator.Send(new GetTaskItemQuery());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTaskItem(int id)
+        {
+            var value = await _mediator.Send(new GetTaskItemByIdQuery(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTaskItem(CreateTaskItemCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Task Item başarıyla eklendi");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveTaskItem(int id)
+        {
+            await _mediator.Send(new RemoveTaskItemCommand(id));
+            return Ok("Task Item başarıyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateTaskItem(UpdateTaskItemCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Task Item başarıyla güncellendi");
+        }
+    }
+}
