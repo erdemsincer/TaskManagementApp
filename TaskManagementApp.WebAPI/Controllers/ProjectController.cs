2e7926d2cfc2c40c1aa6c00d3e2bd2340a55e55e
Erdem <erdemsincer123@icloud.com>
2025-04-10 22:15:54 +0300

    Mediatör api

---
+++ TaskManagementApp.WebAPI/Controllers/ProjectController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementApp.BusinessLayer.Features.Mediator.Commands.ProjectCommand;
+using TaskManagementApp.BusinessLayer.Features.Mediator.Queries.ProjectQueries;
+
+namespace TaskManagementApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProjectController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProjectList()
+        {
+            var values = await _mediator.Send(new GetProjectQuery());
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProject(int id)
+        {
+            var value = await _mediator.Send(new GetProjctByIdQuery(id));
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProject(CreateProjectCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Proje başarıyla eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> RemoveProject(int id)
+        {
+            await _mediator.Send(new RemoveProjectCommand(id));
+            return Ok("Proje başarıyla silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProject(UpdateProjectCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Proje başarıyla güncellendi");
+        }
+    }
+}
--- TaskManagementApp.WebAPI/Program.cs
+++ TaskManagementApp.WebAPI/Program.cs
@@ -1,37 +1,54 @@
 using Microsoft.EntityFrameworkCore;
 using TaskManagementApp.BusinessLayer.Abstract;
 using TaskManagementApp.BusinessLayer.Concrete;
+using TaskManagementApp.BusinessLayer.Features.Mediator.Handlers.ProjectHandlers;
 using TaskManagementApp.DataAccessLayer.Abstract;
 using TaskManagementApp.DataAccessLayer.Contexts;
 using TaskManagementApp.DataAccessLayer.EntityFrameworkCore;
 using TaskManagementApp.DataAccessLayer.Repositories;
+using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Controllers
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+// AutoMapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+// MediatR (CQRS)
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(GetProjectQueryHandler).Assembly);
+});
+
+// Generic Repositories
 builder.Services.AddScoped(typeof(IGenericDal<>), typeof(GenericRepository<>));
 builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
+
+// Services
 builder.Services.AddScoped<IUserService, UserManager>();
 builder.Services.AddScoped<IRoleService, RoleManager>();
 builder.Services.AddScoped<IProjectService, ProjectManager>();
 builder.Services.AddScoped<ITaskItemService, TaskItemManager>();
 builder.Services.AddScoped<ICommentService, CommentManager>();
+
+// DALs
 builder.Services.AddScoped<ICommentDal, EfCommentDal>();
 builder.Services.AddScoped<IUserDal, EfUserDal>();
 builder.Services.AddScoped<IRoleDal, EfRoleDal>();
 builder.Services.AddScoped<IProjectDal, EfProjectDal>();
 builder.Services.AddScoped<ITaskItemDal, EfTaskItemDal>();
 
+var app = builder.Build();
+
+// Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -39,9 +56,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
