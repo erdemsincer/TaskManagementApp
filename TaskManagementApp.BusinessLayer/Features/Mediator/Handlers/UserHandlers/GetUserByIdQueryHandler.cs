3bf7bde7247ea4316e904ee972a409141e3cf0e8
Erdem <erdemsincer123@icloud.com>
2025-04-13 17:02:29 +0300

    Ide göre kullanıcı getirme mediatör

---
+++ TaskManagementApp.BusinessLayer/Features/Mediator/Handlers/UserHandlers/GetUserByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using TaskManagementApp.BusinessLayer.Abstract;
+using TaskManagementApp.BusinessLayer.Features.Mediator.Queries.UserQueries;
+using TaskManagementApp.BusinessLayer.Features.Mediator.Results.UserResult;
+
+namespace TaskManagementApp.BusinessLayer.Features.Mediator.Handlers.UserHandlers
+{
+    public class GetUserByIdQueryHandler:IRequestHandler<GetUserByIdQuery,GetUserByIdQueryResult>
+    {
+        private readonly IUserService _userService;
+
+        public GetUserByIdQueryHandler(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<GetUserByIdQueryResult> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userService.GetByIdAsync(request.Id);
+            if (user == null)
+            {
+                return null;
+            }
+
+            return new GetUserByIdQueryResult
+            {
+                Id = user.Id,
+                FullName = user.FullName,
+                Email = user.Email,
+                RoleId = user.RoleId,
+                RoleName = user.Role?.RoleName,
+                CreatedDate = user.CreatedDate
+            };
+        }
+    }
+}
---
+++ TaskManagementApp.BusinessLayer/Features/Mediator/Queries/UserQueries/GetUserByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using TaskManagementApp.BusinessLayer.Features.Mediator.Results.UserResult;
+
+namespace TaskManagementApp.BusinessLayer.Features.Mediator.Queries.UserQueries
+{
+    public class GetUserByIdQuery: IRequest<GetUserByIdQueryResult>
+    {
+        public int Id { get; set; }
+
+        public GetUserByIdQuery(int ıd)
+        {
+            Id = ıd;
+        }
+    }
+}
